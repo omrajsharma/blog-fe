f3134eb074d8e72e2ee951c99e8833d8cb59d052
omrajsharma <omraj7413@gmail.com>
2023-06-18 09:57:22 +0530

    feat: profile check

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,39 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/api/v1/auth/profile', {
+      credentials: 'include'
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.username) {
+        setUser(data.username)
+      }
+    })
+  }, [])
+
+
   return (
     <header>
         <Link to='/' className='logo'>Our Tech Blogs</Link>
         <nav>
-          <Link to='/'>Home</Link>
-          <Link to='/login'>Login</Link>
-          <Link to='/register'>Register</Link>
+          { user && (
+            <>
+            <Link to='/create'>Create Blog</Link>
+            <Link to='/profile'>Profile</Link>
+            <Link to='/logout'>Logout</Link>
+            </>
+          )}
+          { !user && (
+            <>
+              <Link to='/'>Home</Link>
+              <Link to='/login'>Login</Link>
+              <Link to='/register'>Register</Link>
+            </>
+          )}
         </nav>
     </header>
   )
